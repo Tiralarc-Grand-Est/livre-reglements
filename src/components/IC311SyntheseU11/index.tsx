09d53c24874ae7d9e338b06f8fc57a9e55cf1988
Julien Blatecky <julien.blatecky@tiralarc-grand-est.fr>
2022-07-31 14:58:49 +0200

    Finish poussin

--- src/components/IC311SyntheseU11/index.tsx
+++ src/components/IC311SyntheseU11/index.tsx
@@ -1,51 +1,143 @@
 import React from "react";
+import styles from "./styles.module.css";
 
 export default function IC311SyntheseU11(): JSX.Element {
   return (
     <table>
       <thead>
         <tr>
+          <th rowSpan={2}></th>
+          <th rowSpan={2}>Certificats médicaux</th>
+          <th colSpan={2}>Tir en Salle(Tir à 18m)</th>
+          <th>Tir à l'Arc Extérieur</th>
+          <th colSpan={3}>Disciplines de Parcours</th>
+        </tr>
+        <tr>
+          <th>18m</th>
+          <th>25m</th>
           <th></th>
+          <th>Campagne</th>
+          <th>Nature</th>
+          <th>3D</th>
         </tr>
       </thead>
+      <tbody>
+        <tr>
+          <td>
+            Poussins(U11) jusqu'à <b>7 ans</b>
+          </td>
+          <td>
+            Certificat médical de non contre-indication à la pratique du tir à
+            l’arc (APS) (1)
+          </td>
+          <td>NON</td>
+          <td>NON</td>
+          <td>NON</td>
+          <td>NON</td>
+          <td>NON</td>
+          <td>NON</td>
+        </tr>
+        <tr>
+          <td>
+            Poussins(U11) de <b>8, 9 et 10 ans</b>
+          </td>
+          <td>
+            Avec certificat médical de non contre-indication à la pratique du
+            tir à l’arc en compétition délivré par tout docteur en médecine si
+            inférieur ou égal à 18# marquées et établi par un médecin agréé par
+            la FFTA si plus de 18# marquées
+          </td>
+          <td>OUI (2)</td>
+          <td>NON</td>
+          <td>OUI (2)</td>
+          <td>NON</td>
+          <td>NON</td>
+          <td>NON</td>
+        </tr>
+        <tr>
+          <td colSpan={8} className={styles.separator}></td>
+        </tr>
+        <tr>
+          <td>
+            Poussins(U11) en{" "}
+            <b>dernière année (10 ans) surclassés en Benjamins(U13)</b> (voir
+            ci-dessous)
+          </td>
+          <td rowSpan={8}>
+            Avec certificat médical ci-dessus + certificat médical de
+            surclassement établi par un médecin agréé par la FFTA
+          </td>
+          <td>OUI</td>
+          <td>OUI</td>
+          <td>OUI</td>
+          <td>OUI</td>
+          <td>OUI (dans les divisions d’arc ouvertes aux Benjamins(U13))</td>
+          <td>OUI (dans les divisions d’arc ouvertes aux Benjamins(U13))</td>
+        </tr>
+        <tr>
+          <td>Arc Classique</td>
+          <td>OUI</td>
+          <td>OUI</td>
+          <td>OUI</td>
+          <td>OUI</td>
+          <td className={styles.disabled}></td>
+          <td className={styles.disabled}></td>
+        </tr>
+        <tr>
+          <td>Arc à poulies</td>
+          <td className={styles.disabled}></td>
+          <td className={styles.disabled}></td>
+          <td className={styles.disabled}></td>
+          <td className={styles.disabled}></td>
+          <td className={styles.disabled}></td>
+          <td className={styles.disabled}></td>
+        </tr>
+        <tr>
+          <td>Arc Nu (BB)</td>
+          <td>OUI (2)</td>
+          <td>OUI (2)</td>
+          <td>OUI (2)</td>
+          <td>OUI (2)</td>
+          <td>OUI</td>
+          <td>OUI</td>
+        </tr>
+        <tr>
+          <td>Arc à poulies nu</td>
+          <td className={styles.disabled}></td>
+          <td className={styles.disabled}></td>
+          <td className={styles.disabled}></td>
+          <td className={styles.disabled}></td>
+          <td className={styles.disabled}></td>
+          <td className={styles.disabled}></td>
+        </tr>
+        <tr>
+          <td>Arc Droit (long bow) </td>
+          <td className={styles.disabled}></td>
+          <td className={styles.disabled}></td>
+          <td className={styles.disabled}></td>
+          <td className={styles.disabled}></td>
+          <td className={styles.disabled}></td>
+          <td className={styles.disabled}></td>
+        </tr>
+        <tr>
+          <td>Arc libre</td>
+          <td className={styles.disabled}></td>
+          <td className={styles.disabled}></td>
+          <td className={styles.disabled}></td>
+          <td className={styles.disabled}></td>
+          <td className={styles.disabled}></td>
+          <td className={styles.disabled}></td>
+        </tr>
+        <tr>
+          <td>Arc Chasse</td>
+          <td className={styles.disabled}></td>
+          <td className={styles.disabled}></td>
+          <td className={styles.disabled}></td>
+          <td className={styles.disabled}></td>
+          <td className={styles.disabled}></td>
+          <td className={styles.disabled}></td>
+        </tr>
+      </tbody>
     </table>
   );
 }
-
-/*
-                                                           Tir en Salle(Tir à   Tir à l'Arc           Disciplines de Parcours
-                            Certificats médicaux                 18m)           Extérieur
-                                                           18m         25m                    Campagne       Nature             3D
-                           Certificat médical de non
-
-Poussins(U11)
-contre-indication à la pratique NON NON NON NON NON NON
-Jusqu'à 7ans
-du tir à l’arc (APS) (1)
-Avec certificat médical de
-non contre-indication à la
-pratique du tir à l’arc en
-Poussins(U11) de compétition délivré par tout
-docteur en médecine si OUI (2) NON OUI (2) NON NON NON
-8, 9 et 10 ans
-inférieur ou égal à 18#
-marquées et établi par un
-médecin agréé par la FFTA
-si plus de 18 # marquées
-
-Poussins(U11) en
-dernière année (10 OUI OUI
-(dans les (dans les
-ans)
-surclassés en Avec certificat médical OUI OUI OUI OUI divisions d’arc divisions d’arc
-ci-dessus ouvertes aux ouvertes aux
-Benjamins(U13) Benjamins(U13)) Benjamins(U13))
-(voir ci-dessous) +
-Arc Classique certificat médical de OUI OUI OUI OUI
-Arc à poulies
-Arc Nu (BB) surclassement établi par un OUI (2) OUI (2) OUI (2) OUI(2) OUI OUI
-Arc à poulies nu
-Arc Droit (long bow) médecin agréé par la FFTA
-Arc libre
-Arc Chasse
-*/
